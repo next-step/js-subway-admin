196670159cfe7ff6914379bd90e22ce5955bcdd8
JunilHwang <junil.h@kakao.com>
2021-06-06 15:01:06 +0900

    feat: core class init

---
+++ src/@core/Component.ts
@@ -0,0 +1,73 @@
+import {observable, observe} from "./";
+
+export abstract class Component<State = {}, Props = {}> {
+
+  protected $state: State = {} as State;
+  protected $components: Record<string, Component | Component[]> = {};
+  private isRoot = false;
+
+  constructor(
+    protected readonly $target: HTMLElement,
+    protected readonly $props: Props = {} as Props,
+  ) {
+    this.setup();
+    this.$state = observable<State>(this.$state!);
+    observe(() => this.render());
+    this.setEvent();
+    this.mounted();
+  }
+  protected setup() {}
+  protected mounted() {}
+  protected updated() {}
+  protected initChildComponent(el: HTMLElement, componentName: string) { }
+  protected abstract template(): string;
+  protected setEvent() {}
+
+  protected addEvent (eventType: string, selector: string, callback: Function) {
+    this.$target.addEventListener(eventType, (e) => {
+      const target = e.target as HTMLElement;
+      const currentTarget = e.currentTarget as HTMLElement;
+      const checked = target.closest(selector) || [ ...currentTarget.querySelectorAll(selector) ].includes(target);
+      if (!checked) return;
+      callback(e);
+    })
+  }
+
+  private render() {
+    this.$components = {};
+
+    this.$target.innerHTML = this.template();
+    this.$target.querySelectorAll('[data-component]')
+                .forEach(el => this.setupChildComponent(el));
+
+    this.updated();
+  }
+
+  private setupChildComponent(el: Element) {
+    if (!(el instanceof HTMLElement)) return;
+    if (!this.initChildComponent) return;
+
+    const name: string = el.dataset.component!;
+    const childComponent = this.initChildComponent(el, name) as Component | undefined;
+    if (!childComponent) return;
+
+    const {$components} = this;
+    if (!$components[name]) {
+      $components[name] = childComponent;
+      return;
+    }
+    if ($components[name] instanceof Component) {
+      $components[name] = [
+        $components[name] as Component,
+        childComponent
+      ];
+      return;
+    }
+    if (Array.isArray($components[name])) {
+      ($components[name] as Component[]).push(childComponent);
+      return;
+    }
+  }
+
+  public setRoot() { this.isRoot = true; }
+}
---
+++ src/@core/Observer.ts
@@ -0,0 +1,31 @@
+let currentObserver: Function | null = null;
+
+export function observe (fn: Function) {
+  currentObserver = fn;
+  fn();
+  currentObserver = null;
+}
+
+export function observable<T> (obj: T): T {
+
+  if (!(obj instanceof Object)) return obj;
+
+  Object.entries(obj).forEach(([ key, value ]) => {
+    const observers: Set<Function> = new Set();
+    let _value = value;
+
+    Object.defineProperty(obj, key, {
+      get () {
+        observers.add(currentObserver!);
+        return _value;
+      },
+
+      set (newValue) {
+        _value = newValue;
+        observers.forEach(fn => fn?.());
+      },
+    })
+  });
+
+  return obj;
+}
