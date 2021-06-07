1c8c97201ff97bfacdcbeb36f9b237ec69fcd97c
JunilHwang <junil.h@kakao.com>
2021-06-07 22:54:11 +0900

    feat: 지하철역 삭제 작업 완료

---
+++ src/pages/stations/StationItem.ts
@@ -0,0 +1,42 @@
+import {Component} from "~@core";
+
+interface StationItemsProps {
+  name: string;
+  editStation: () => void;
+  removeStation: () => void;
+}
+
+export class StationItem extends Component<{}, StationItemsProps> {
+
+  protected template(): string {
+    return `
+      <div class="station-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2">${this.$props.name}</span>
+        <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 update">
+          수정
+        </button>
+        <button type="button" class="bg-gray-50 text-gray-500 delete">
+          삭제
+        </button>          
+      </div>
+      <hr class="my-0" />
+    `;
+  }
+
+  protected setEvent() {
+
+    this.addEvent('click', '.update', (event: MouseEvent) => {
+      event.preventDefault();
+      this.$props.editStation();
+    });
+
+    this.addEvent('click', '.delete', (event: MouseEvent) => {
+      event.preventDefault();
+
+      if (confirm('정말로 삭제하시겠습니까?')) {
+        this.$props.removeStation();
+      }
+    });
+
+  }
+}
