b597f2c6487f4a3f3a2dffbbb348ead11cef2010
bcround <sbc1142@gmail.com>
2021-06-07 17:28:17 +0900

    Feat: template들 export 해주는 index.ts 생성 new file:   src/js/templates/index.ts

---
+++ src/js/templates/index.ts
@@ -0,0 +1,6 @@
+export { default as headerTemplate } from './header';
+export { default as stationsTemplate } from './stations';
+export { default as linesTemplate } from './lines';
+export { default as sectionsTemplate } from './sections';
+export { default as loginTemplate } from './login';
+export { default as homeTemplate } from './home';
