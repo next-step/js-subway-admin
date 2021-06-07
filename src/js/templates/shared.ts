25c33d1af52ddd6c752c8ca88d0b2302bf753de0
bcround <sbc1142@gmail.com>
2021-06-07 17:29:22 +0900

    Feat: 중복되는 템플릿들 shared라는 파일안에 생성 new file:   src/js/templates/shared.ts

---
+++ src/js/templates/shared.ts
@@ -0,0 +1,43 @@
+export const getStationsListsTemplate = (station: string): string => `
+<li class="station-list-item d-flex items-center py-2">
+  <span class="w-100 pl-2">${station}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm"
+  >
+    삭제
+  </button>
+  </li>
+  <hr class="my-0" />
+`;
+
+export const getLinesListsTemplate = (line: string): string => `
+<li class="d-flex items-center py-2 relative">
+  <span class="subway-line-color-dot bg-blue-400"></span>
+  <span class="w-100 pl-6 subway-line-list-item-name"
+    >${line}</span
+  >
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm"
+  >
+    삭제
+  </button>
+</li>
+<hr class="my-0" />
+`;
+
+export const getOptionsTemplate = (data: string): string =>
+  `<option>${data}</option>`;
