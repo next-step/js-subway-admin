e67110e180d3c66a4541c329d73b0ec12eb47f13
bcround <sbc1142@gmail.com>
2021-06-09 03:21:56 +0900

    Feat: page별로 다른 모달 렌더 해주는 함수와 snack bar 구현 modified:   index.html new file:   src/css/ui/modules/snackBar.css modified:   src/js/utils/render.ts

--- src/js/utils/render.ts
+++ src/js/utils/render.ts
@@ -6,8 +6,16 @@ import {
   sectionsTemplate,
   loginTemplate,
   homeTemplate,
-  pageNotFoundTemplate
+  pageNotFoundTemplate,
+  linesModalTemplate,
+  sectionsModalTemplate,
+  stationsModalTemplate
 } from '../templates/index';
+import $ from './dom';
+import { _some } from './_';
+
+const $snackBar = $('#snackBar');
+const $modal = $('.modal');
 
 const pageTemplates: StringKeyObject<() => string> = {
   [PagesPath.HOME]: homeTemplate,
@@ -18,10 +26,29 @@ const pageTemplates: StringKeyObject<() => string> = {
   [PagesPath.PAGENOTFOUND]: pageNotFoundTemplate
 };
 
+const modalTemplates: StringKeyObject<() => string> = {
+  [PagesPath.STATIONS]: stationsModalTemplate,
+  [PagesPath.LINES]: linesModalTemplate,
+  [PagesPath.SECTIONS]: sectionsModalTemplate
+};
+
 export const render = (template: string, node: HTMLElement): void => {
   node.innerHTML = template;
 };
 
 export const renderPages = (path: string, node: HTMLElement): void => {
   node.innerHTML = pageTemplates[path]();
+};
+
+export const renderModals = (path: string): void => {
+  if (!_some(Object.keys(modalTemplates), key => path === key)) return;
+  $modal.innerHTML = modalTemplates[path]();
+};
+
+export const renderSnackBar = (message: string): void => {
+  $snackBar.innerText = message;
+  $snackBar.classList.toggle('show');
+  setTimeout(() => {
+    $snackBar.classList.toggle('show');
+  }, 3000);
 };
