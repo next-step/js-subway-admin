fca8280509047c86cc3f7f6c8f2a7a307ee3da37
moonee <hellomooneekim@gmail.com>
2021-06-08 16:33:08 +0900

    feat : 오버레이/버튼 클릭시 모달 닫기 구현

--- src/js/components/Modal/index.ts
+++ src/js/components/Modal/index.ts
@@ -13,13 +13,20 @@ class Modal extends Component {
   }
 
   protected bindEvents(): void {
-    // 닫기 버튼, overlay 버튼에 모달 close 구현
+    this.$container.addEventListener("click", (e: Event) => {
+      if (!this.$container.classList.contains("open")) return;
+      const target = e.target as HTMLElement;
+      const id = target.id;
+      if (id !== "modal" && id !== "modal-close") return;
+      uiService.closeModal();
+    });
   }
 
   protected componentMount(): void {
     const { isModalOpen, modalContents, modalTitle } = uiStore.getState();
     this.$innerContainer = $("#modal-contents", this.$container);
     this.$titleContainer = $("#modal-title", this.$container);
+
     if (isModalOpen) {
       this.$container.classList.add("open");
       this.$titleContainer.innerText = modalTitle;
