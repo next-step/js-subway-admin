357daed2f281c772b5e48629816f1d5dfa4d6aa8
moonee <hellomooneekim@gmail.com>
2021-06-08 15:06:55 +0900

    chore : 바벨 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-runtime"],
-          },
         },
       },
       {
