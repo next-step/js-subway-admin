e40fadbbd30aa19778bf2a8866b68ee3121bcf52
junilhwang <tjsdlf4261@naver.com>
2021-06-08 16:16:59 +0900

    refactor: Dom Diff 알고리즘 적용 완료

--- src/@core/Component.ts
+++ src/@core/Component.ts
@@ -1,4 +1,5 @@
-import {observable, observe} from "./";
+import {applyDomDiff, observable, observe} from "./";
+import {selectAll} from "~utils";
 
 export abstract class Component<State = {}, Props = {}> {
 
@@ -26,20 +27,22 @@ export abstract class Component<State = {}, Props = {}> {
   protected abstract template(): string;
   protected setEvent() {}
 
-  protected addEvent (eventType: string, selector: string, callback: Function) {
-    this.$target.addEventListener(eventType, (e) => {
-      const target = e.target as HTMLElement;
-      const currentTarget = e.currentTarget as HTMLElement;
-      const checked = target.closest(selector) || [ ...currentTarget.querySelectorAll(selector) ].includes(target);
-      if (!checked) return;
-      callback(e);
-    })
+  protected addEvent (eventType: string, selector: string, callback: (e: Event) => void) {
+    selectAll(selector, this.$target)
+      .forEach(el => {
+        el.removeEventListener(eventType, callback);
+        el.addEventListener(eventType, callback);
+      });
   }
 
   private render() {
     this.$components = {};
 
-    this.$target.innerHTML = this.template();
+    const $target = this.$target.cloneNode(true) as HTMLElement;
+    $target.innerHTML = this.template();
+
+    applyDomDiff(this.$target, $target);
+
     this.$target.querySelectorAll('[data-component]')
                 .forEach(el => this.setupChildComponent(el));
 
--- src/@core/DomDiff.ts
+++ src/@core/DomDiff.ts
@@ -1,3 +1,63 @@
-export const applyDomDiff = () => {
+function replaceAttributes (realNode: Element, virtualNode: Element): void {
+
+  for (const { name, value } of [ ...realNode.attributes ]) {
+    const virtualAttribute = virtualNode.getAttribute(name);
+    if (value === virtualAttribute) continue;
+    if (!virtualAttribute) {
+      realNode.removeAttribute(name);
+      continue;
+    }
+    realNode.setAttribute(name, virtualAttribute);
+  }
+
+  for (const { name, value } of [ ...virtualNode.attributes ]) {
+    if (!realNode.getAttribute(name)) {
+      realNode.setAttribute(name, value);
+    }
+  }
+
+}
+
+function applyDomDiffDeep (parentNode: Element, realNode: ChildNode, virtualNode: ChildNode): void {
+  if (realNode instanceof Text && virtualNode instanceof Text && realNode.nodeValue !== virtualNode.nodeValue) {
+    realNode.nodeValue = virtualNode.nodeValue;
+    return;
+  }
+
+  if (realNode && !virtualNode) {
+    return realNode.remove();
+  }
+
+  if (!realNode && virtualNode) {
+    parentNode.appendChild(virtualNode);
+    return;
+  }
+
+  if ((typeof realNode) !== (typeof virtualNode) || realNode.nodeName !== virtualNode.nodeName) {
+    parentNode.removeChild(realNode);
+    parentNode.appendChild(virtualNode);
+    return;
+  }
+
+  if (!(realNode instanceof Text || virtualNode instanceof Text)) {
+    replaceAttributes(realNode as Element, virtualNode as Element);
+  }
+
+  const realChildren: ChildNode[] = [ ...realNode.childNodes ];
+  const virtualChildren: ChildNode[] = [ ...virtualNode.childNodes ];
+
+  Array(Math.max(realChildren.length, virtualChildren.length))
+    .fill(0)
+    .forEach((v, k) => applyDomDiffDeep(realNode as Element, realChildren[k], virtualChildren[k]));
+}
+
+export function applyDomDiff (realNode: Element, virtualNode: Element) {
+
+  const realChildren: ChildNode[] = [ ...realNode.childNodes ];
+  const virtualChildren: ChildNode[] = [ ...virtualNode.childNodes ];
+
+  Array(Math.max(realChildren.length, virtualChildren.length))
+    .fill(0)
+    .forEach((v, k) => applyDomDiffDeep(realNode, realChildren[k], virtualChildren[k]));
 
 }
