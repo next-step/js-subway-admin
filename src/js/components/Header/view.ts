9c9fad79d3841459f391af6f1d0cdd5576255a27
moonee <hellomooneekim@gmail.com>
2021-06-08 10:27:59 +0900

    feat : 로그인 상태에 따라 헤더 뷰 변경

--- src/js/components/Header/view.ts
+++ src/js/components/Header/view.ts
@@ -4,15 +4,20 @@ const view = (isLoggedIn: boolean): string => {
     <h1 class="text-center font-bold">🚇 지하철 노선도</h1>
    </a>
   <nav class="d-flex justify-center flex-wrap">
-    <a href="/stations" class="my-1">
-      <button class="btn bg-white shadow mx-1">🚉 역 관리</button>
-    </a>
-    <a href="/lines" class="my-1">
-      <button class="btn bg-white shadow mx-1">🛤️ 노선 관리</button>
-    </a>
-    <a href="/sections" class="my-1">
-      <button class="btn bg-white shadow mx-1">🔁 구간 관리</button>
-    </a>
+  ${
+    isLoggedIn
+      ? `
+  <a href="/stations" class="my-1">
+    <button class="btn bg-white shadow mx-1">🚉 역 관리</button>
+  </a>
+  <a href="/lines" class="my-1">
+    <button class="btn bg-white shadow mx-1">🛤️ 노선 관리</button>
+  </a>
+  <a href="/sections" class="my-1">
+    <button class="btn bg-white shadow mx-1">🔁 구간 관리</button>
+  </a>`
+      : ""
+  }
     ${
       isLoggedIn
         ? `
