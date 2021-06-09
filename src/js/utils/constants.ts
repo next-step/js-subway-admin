6affbd22fccfb6c26450f871045ba3111241cd4e
bcround <sbc1142@gmail.com>
2021-06-09 17:57:42 +0900

    Feat: 메세지들 enum 타입으로 정의 modified:   src/js/utils/constants.ts

--- src/js/utils/constants.ts
+++ src/js/utils/constants.ts
@@ -6,3 +6,11 @@ export enum PagesPath {
   LOGIN = '/pages/login',
   PAGENOTFOUND = '/page-not-found'
 }
+
+export enum Message {
+  DELETE_CONFIRM = '정말로 해당 역을 삭제하시겠습니까?',
+  CANNOT_DELETE_REGISTERED_STATION = '해당 역은 이미 노선에 등록되어있어서 삭제가 불가능합니다.',
+  DELETE_STATION = '해당 역이 삭제되었습니다.',
+  DUPLICATE_STATION = '중복된 역 이름입니다.',
+  DUPLICATE_LINE = '중복된 노선 이름입니다.'
+}
