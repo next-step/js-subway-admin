74dca1043d7fbc42bc3333ffb91c4ad50c72ad88
bcround <sbc1142@gmail.com>
2021-06-09 17:17:14 +0900

    Feat: filter와 eact 타입 변경 및 reduce 추가 modified:   src/js/utils/_.ts

--- src/js/utils/_.ts
+++ src/js/utils/_.ts
@@ -1,22 +1,31 @@
-export const _each = <T>(list: T[], iter: (val: T) => void): void => {
+export const _each = <T>(
+  list: T[],
+  iter: (val: T, idx?: number) => void
+): void => {
   for (let i = 0; i < list.length; i++) {
-    iter(list[i]);
+    iter(list[i], i);
   }
 };
 
-export const _map = <T, U>(list: T[], mapper: (val: T) => U): U[] => {
+export const _map = <T, U>(
+  list: T[],
+  mapper: (val: T, idx?: number) => U
+): U[] => {
   const newList: U[] = [];
 
-  _each<T>(list, (val: T) => {
-    newList.push(mapper(val));
+  _each<T>(list, (val: T, idx?: number) => {
+    newList.push(mapper(val, idx));
   });
   return newList;
 };
 
-export const _filter = <T>(list: T[], predi: (val: T) => boolean): T[] => {
+export const _filter = <T, U>(
+  list: T[] | U,
+  predi: (val: T) => boolean
+): T[] => {
   const newList: T[] = [];
 
-  _each(list, (val: T) => {
+  _each(list as T[], (val: T) => {
     if (predi(val)) newList.push(val);
   });
   return newList;
@@ -27,4 +36,15 @@ export const _some = <T>(list: T[], predi: (val: T) => boolean): boolean => {
     if (predi(list[i])) return true;
   }
   return false;
+};
+
+export const _reduce = <T, U>(
+  list: T[],
+  red: (acc: U, val: T, idx: number) => void,
+  acc: U
+): U => {
+  for (let i = 0; i < list.length; i++) {
+    red(acc, list[i], i);
+  }
+  return acc;
 };
