accdffcb1ab46ea458cb5ece077a6dc51e4a1972
bcround <sbc1142@gmail.com>
2021-06-09 19:33:09 +0900

    Feat: 메세지 추가 modified:   src/js/utils/constants.ts

--- src/js/utils/constants.ts
+++ src/js/utils/constants.ts
@@ -8,9 +8,11 @@ export enum PagesPath {
 }
 
 export enum Message {
-  DELETE_CONFIRM = '정말로 해당 역을 삭제하시겠습니까?',
+  DELETE_STATION_CONFIRM = '정말로 해당 역을 삭제하시겠습니까?',
   CANNOT_DELETE_REGISTERED_STATION = '해당 역은 이미 노선에 등록되어있어서 삭제가 불가능합니다.',
   DELETE_STATION = '해당 역이 삭제되었습니다.',
   DUPLICATE_STATION = '중복된 역 이름입니다.',
+  DELETE_LINE_CONFIRM = '정말로 해당 노선을 삭제하시겠습니까?',
+  DELETE_LINE = '해당 노선이 삭제되었습니다.',
   DUPLICATE_LINE = '중복된 노선 이름입니다.'
 }
